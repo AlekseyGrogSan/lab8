4a2049697622118e33a5a9355ce9681cfb3a5435
Алексей Барвинский <alexkernel05@gmail.com>
2024-10-05 08:48:07 +0300

    fourth_commit

--- time.pas
+++ time.pas
@@ -2,9 +2,11 @@
   var year:= readinteger('Введите год:');
   
   if (year mod 100 = 0) and (year mod 400 <> 0) then
-     print('Год невисокосный')
+     print('Год невисокосный, в нем 365 дней')
   else if (year mod 4 = 0) then
-     print('Год високосный')
+     print('Год високосный, в нем 366 дней')
   else
-     print('Год не високосный')
+     print('Год не високосный, в нем 365 дней')
+  if year = 0 then
+    print('Года не существует!')
 end.
---
+++ timer.pas
@@ -0,0 +1,4 @@
+﻿begin
+  var (h, min, sec):= readinteger3('Введите часы, минуты и секунды:');
+  print($'Кол-во секунд: {h*3600 + min*60 + sec}')
+end.
