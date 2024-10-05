8232ed69097a9ee05ae261e1fd2481b74c78dc8f
Алексей Барвинский <alexkernel05@gmail.com>
2024-10-05 08:39:48 +0300

    Commit three

---
+++ time.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var year:= readinteger('Введите год:');
+  
+  if (year mod 100 = 0) and (year mod 400 <> 0) then
+     print('Год невисокосный')
+  else if (year mod 4 = 0) then
+     print('Год високосный')
+  else
+     print('Год не високосный')
+end.
